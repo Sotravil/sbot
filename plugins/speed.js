9c8d220aa5fe315dd37b6e550b584e4da7d2222b
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-07-19 14:59:57 -0500

    Update speed.js

--- plugins/speed.js
+++ plugins/speed.js
@@ -42,7 +42,7 @@ let handler = async (m, { conn }) => {
   let neww = performance.now()
   let speed = neww - old
   let txt = `
-Merespon dalam ${speed} millidetik
+Responde en ${speed} milisegundos
 
 💬 Status :
 - *${groups.length}* Group Chats
