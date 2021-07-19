540bbd429b7058da17d566233fcbd3cd0d71f092
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-07-19 15:03:34 -0500

    Update gay.js

--- plugins/gay.js
+++ plugins/gay.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/gay', {
     avatar: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }), 'gay.png', '@user eri gay', m)
+  }), 'gay.png', 'es gay', m)
 }
 
 handler.help = ['gay']
